776ff0e875e744d50642c0980ad53fb269302890
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2019-12-10 00:39:39 +0100

    Comments

--- src/node_stack.h
+++ src/node_stack.h
@@ -28,6 +28,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 #include "tree.h"
 
+/* The NodeStack elements are constituted of Node/Edge pairs */
 typedef struct __NodeStackElt NodeStackElt;
 typedef struct __NodeStackElt {
   Node *node;
@@ -42,9 +43,14 @@ typedef struct __NodeStack {
 } NodeStack;
 
 
+/* Initialize a new Node Stack*/
 NodeStack *new_nodestack();
+/* Pushes a new node/edge pair to the Stack */
 void nodestack_push(NodeStack *ns, Node *n, Edge *e);
+/* Pops and returns the head of the Stack. The calling function is
+ responsible for freeing the elt: free(elt).*/
 NodeStackElt * nodestack_pop(NodeStack *ns);
+/* frees the whole stack and all its elements */
 void nodestack_free(NodeStack *ns);
 
 #endif
